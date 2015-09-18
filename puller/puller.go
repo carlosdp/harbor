de9b0c2fb912aab9f68635f620fbf898685ff278
Carlos Diaz-Padron <cpadron@mozilla.com>
2015-09-17 21:06:31 -0700

    Refactor and add docs to puller package

--- puller/puller.go
+++ puller/puller.go
@@ -2,23 +2,29 @@ package puller
 
 import (
 	"errors"
+
 	"github.com/carlosdp/harbor/chain"
 )
 
+// RegisteredPullers contains the pullers registered with the Harbor build.
 var RegisteredPullers = make(map[string]Puller)
 
+// Puller describes an interface for a Harbor Puller.
 type Puller interface {
 	New() Puller
 	Pull(uri string) (string, error)
 }
 
-type PullerWrapper struct {
+// Wrapper is a wrapper struct for holding a
+// name Puller in the registry.
+type Wrapper struct {
 	name   string
 	Puller Puller
 }
 
-func NewPuller(name string, puller Puller) *PullerWrapper {
-	pullerWrap := &PullerWrapper{
+// NewPuller wraps a builder and returns a puller Wrapper.
+func NewPuller(name string, puller Puller) *Wrapper {
+	pullerWrap := &Wrapper{
 		name:   name,
 		Puller: puller,
 	}
@@ -26,11 +32,13 @@ func NewPuller(name string, puller Puller) *PullerWrapper {
 	return pullerWrap
 }
 
-func (p *PullerWrapper) Name() string {
+// Name returns the name of the puller.
+func (p *Wrapper) Name() string {
 	return p.name
 }
 
-func (p *PullerWrapper) Execute(d chain.Deployment) error {
+// Execute runs the pull operation for a deployment chain.
+func (p *Wrapper) Execute(d chain.Deployment) error {
 	workDir, err := p.Puller.Pull(d.URI())
 	if err != nil {
 		return err
@@ -41,14 +49,18 @@ func (p *PullerWrapper) Execute(d chain.Deployment) error {
 	return nil
 }
 
-func (p *PullerWrapper) Rollback() error {
+// Rollback does nothing at the moment in a puller.
+func (p *Wrapper) Rollback() error {
 	return nil
 }
 
+// RegisterPuller registers a puller with `name`.
 func RegisterPuller(name string, puller Puller) {
 	RegisteredPullers[name] = puller
 }
 
+// GetPuller returns a puller registered as `name`, if it exists.
+// It returns an error if it does not exist.
 func GetPuller(name string) (Puller, error) {
 	puller, ok := RegisteredPullers[name]
 	if !ok {
