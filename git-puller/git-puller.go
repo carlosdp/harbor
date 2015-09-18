a1ac15ef18bd332572e5d0b1f56336ee7b91cbfc
Carlos Diaz-Padron <cpadron@mozilla.com>
2015-09-17 20:50:27 -0700

    Do not export gitPuller struct

--- git-puller/git-puller.go
+++ git-puller/git-puller.go
@@ -9,19 +9,19 @@ import (
 	git "gopkg.in/libgit2/git2go.v22"
 )
 
-type GitPuller struct {
+type gitPuller struct {
 	workDir string
 }
 
 func init() {
-	puller.RegisterPuller("git-puller", &GitPuller{})
+	puller.RegisterPuller("git-puller", &gitPuller{})
 }
 
-func (gp *GitPuller) New() puller.Puller {
-	return &GitPuller{}
+func (gp *gitPuller) New() puller.Puller {
+	return &gitPuller{}
 }
 
-func (gp *GitPuller) Pull(url string) (string, error) {
+func (gp *gitPuller) Pull(url string) (string, error) {
 	timeStamp := strconv.FormatInt(time.Now().Unix(), 10)
 	path := os.TempDir() + timeStamp
 	repo, err := git.Clone(url, path, &git.CloneOptions{})
